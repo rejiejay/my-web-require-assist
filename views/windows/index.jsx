eb9607f6a5f03a49ae6b33febb817b56672af1ec
rejiejay <454766952@qq.com>
2020-05-05 11:28:54 +0800

    add expandRandomHandle

--- views/windows/index.jsx
+++ views/windows/index.jsx
@@ -73,6 +73,58 @@ class MainComponent extends React.Component {
 
     expandAllHandle() {
         this.mindInstan.expand_all()
+        this.colorRestoration()
+    }
+
+    collapseAllHandle() {
+        const self = this
+        this.mindData.data.filter(element => element.id !== 1).map(element => self.mindInstan.collapse_node(element.id))
+        this.colorRestoration()
+    }
+
+    colorRestoration() {
+        const self = this
+        this.mindData.data.filter(element => element.id !== 1).map(element => {
+            const bgcolor = '#428bca'
+            const fgcolor = '#FFF'
+            self.mindInstan.set_node_color(element.id, bgcolor, fgcolor)
+        })
+    }
+
+    expandRandomHandle() {
+        const self = this
+        const shuffle = mindArray => {
+            let len = mindArray.length;
+            for (let i = 0; i < len - 1; i++) {
+                let index = parseInt(Math.random() * (len - i));
+                let temp = mindArray[index];
+                mindArray[index] = mindArray[len - i - 1];
+                mindArray[len - i - 1] = temp;
+            }
+            return mindArray;
+        }
+        const mindData = this.mindData.data.filter(element => element.id !== 1)
+
+        const randomNode = shuffle(mindData)[0]
+
+        let depthList = []
+        const findParent = node => {
+            depthList.push(node.id)
+
+            if (node.parentid !== 1) {
+                const parentNode = mindData.find(element => element.id === node.parentid);
+                findParent(parentNode)
+            }
+        }
+        findParent(randomNode)
+
+        this.collapseAllHandle()
+
+        const bgcolor = '#f1c40f'
+        const fgcolor = '#FFF'
+        self.mindInstan.set_node_color(randomNode.id, bgcolor, fgcolor)
+
+        depthList.reverse().map(id => self.mindInstan.expand_node(id))
     }
 
     render() {
@@ -86,7 +138,9 @@ class MainComponent extends React.Component {
                         >展开所有</div>
                     </div>
                     <div className="operation-item">
-                        <div className="operation-item-container flex-center">随机查看</div>
+                        <div className="operation-item-container flex-center"
+                            onClick={this.expandRandomHandle.bind(this)}
+                        >随机查看</div>
                     </div>
                 </div>
             </div>,
