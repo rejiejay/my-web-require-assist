55353f2ceebce6cb0744d4815b40ca3302937e16
rejiejay <454766952@qq.com>
2020-05-05 08:39:03 +0800

    add windows operation

--- views/windows/index.jsx
+++ views/windows/index.jsx
@@ -14,7 +14,19 @@ class MainComponent extends React.Component {
     render() {
 
         return [
-            <div></div>
+            <div className="operation">
+                <div className="operation-container flex-start">
+                    <div className="operation-item">
+                        <div className="operation-item-container flex-center">展开所有</div>
+                    </div>
+                    <div className="operation-item">
+                        <div className="operation-item-container flex-center">随机查看</div>
+                    </div>
+                </div>
+            </div>,
+
+            <div className="mind">
+            </div>
         ]
     }
 }
